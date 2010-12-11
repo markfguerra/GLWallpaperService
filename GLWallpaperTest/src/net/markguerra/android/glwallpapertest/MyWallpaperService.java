127f258be4a55699ccaabb12df687c7cd6c4e7e9
Mark F Guerra <markfguerra@gmail.com>
2010-12-11 11:35:37 -0500

    Added MyWallpaperService code as it appears on Robert Green's website as of today, Dec 11 2010

---
+++ GLWallpaperTest/src/net/markguerra/android/glwallpapertest/MyWallpaperService.java
@@ -0,0 +1,29 @@
+package net.markguerra.android.glwallpapertest;
+
+public class MyWallpaperService extends GLWallpaperService {
+public MyWallpaperService() {
+super();
+}
+public Engine onCreateEngine() {
+MyEngine engine = new MyEngine();
+return engine;
+}
+
+// prefs and sensor interface is optional, just showing that this is where you do all of that - everything that would normally be in an activity is in here.
+class MyEngine extends GLEngine implements SharedPreferences.OnSharedPreferenceChangeListener, SensorEventListener {
+MyRenderer renderer;
+public MyEngine() {
+super();
+// handle prefs, other initialization
+renderer = new MyRenderer();
+setRenderer(renderer);
+setRenderMode(RENDERMODE_WHEN_DIRTY);
+}
+
+public void onDestroy() {
+super.onDestroy();
+if (renderer != null) {
+renderer.release(); // assuming yours has this method - it should!
+}
+renderer = null;
+}
