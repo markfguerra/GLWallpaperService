9ce647aec863dc5450b2c575edcce8e75461848e
Mark F Guerra <markfguerra@gmail.com>
2010-12-11 12:02:35 -0500

    Added class MyRenderer. Currently bare bones.

---
+++ GLWallpaperTest/src/net/markguerra/android/glwallpapertest/MyRenderer.java
@@ -0,0 +1,25 @@
+package net.markguerra.android.glwallpapertest;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import net.rbgrn.android.glwallpaperservice.*;
+
+public class MyRenderer implements GLWallpaperService.Renderer {
+
+	public void onDrawFrame(GL10 gl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
