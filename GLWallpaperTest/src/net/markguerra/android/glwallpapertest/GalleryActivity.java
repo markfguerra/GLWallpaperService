06d3fdbf105709ce2685cdc347b91b3abe90ea6f
Mark F Guerra <markfguerra@gmail.com>
2011-12-12 22:02:59 -0500

    Added a GLSurfaceView to GalleryActivity. Attached MyRenderer using RendererAdapter to demonstrate usage.

--- GLWallpaperTest/src/net/markguerra/android/glwallpapertest/GalleryActivity.java
+++ GLWallpaperTest/src/net/markguerra/android/glwallpapertest/GalleryActivity.java
@@ -1,12 +1,44 @@
 package net.markguerra.android.glwallpapertest;
 
+import net.rbgrn.android.glwallpaperservice.RendererAdapter;
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 public class GalleryActivity extends Activity {
+	GLSurfaceView glView;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		//Instantiate OpenGL drawing surface and hold a reference
+		ViewGroup container = (ViewGroup)findViewById(R.id.container);
+		glView = new GLSurfaceView(this);
+		MyRenderer renderer = new MyRenderer();
+		glView.setRenderer(new RendererAdapter(renderer));
+
+		//Put the container on the screen for the world to see.
+		container.addView(glView);
+	}
+
+	/**
+	 * Allows GLSurfaceView to respond to the Resume event
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		glView.onResume();
+	}
+
+	/**
+	 * Allows GLSurfaceView to respond to the Pause event
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		glView.onPause();
 	}
 }
