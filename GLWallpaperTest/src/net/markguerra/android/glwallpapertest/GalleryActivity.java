796895710fe3ea6000ff9eb5cef76f6a6c9380fd
Mark F Guerra <markfguerra@gmail.com>
2011-12-12 22:01:48 -0500

    Added GalleryActivity

---
+++ GLWallpaperTest/src/net/markguerra/android/glwallpapertest/GalleryActivity.java
@@ -0,0 +1,12 @@
+package net.markguerra.android.glwallpapertest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GalleryActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
