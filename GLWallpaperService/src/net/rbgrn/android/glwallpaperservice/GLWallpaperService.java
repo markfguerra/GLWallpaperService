9b2c4ecf210044df28a17b23e6e6dd4116c46977
Jay Weisskopf <jay@jayschwa.net>
2012-11-16 13:55:30 -0600

    Deprecate redundant interfaces
    Many of the interfaces are exact duplicates of ones from GLSurfaceView
    and, as far as I can tell, add no value. While integrating the latest
    code into the downstream Rajawali framework, I found these interfaces to
    be a nuisance. Our classes, which already implement the GLSurfaceView
    interfaces, were not considered valid to the GLWallpaperService.
    
    GLWallpaperService has been modified to accept the GLSurfaceView
    interfaces. To maintain backwards compatibility, the redundant
    GLWallpaperService interfaces have been changed to empty extensions of
    their GLSurfaceView counterparts, but marked as deprecated. The downside
    of keeping the interfaces for backwards compatibility is that
    "GLSurfaceView." had to be prefixed onto many declarations.

--- GLWallpaperService/src/net/rbgrn/android/glwallpaperservice/GLWallpaperService.java
+++ GLWallpaperService/src/net/rbgrn/android/glwallpaperservice/GLWallpaperService.java
@@ -50,9 +50,9 @@ public class GLWallpaperService extends WallpaperService {
 		public final static int RENDERMODE_CONTINUOUSLY = 1;
 
 		private GLThread mGLThread;
-		private EGLConfigChooser mEGLConfigChooser;
-		private EGLContextFactory mEGLContextFactory;
-		private EGLWindowSurfaceFactory mEGLWindowSurfaceFactory;
+		private GLSurfaceView.EGLConfigChooser mEGLConfigChooser;
+		private GLSurfaceView.EGLContextFactory mEGLContextFactory;
+		private GLSurfaceView.EGLWindowSurfaceFactory mEGLWindowSurfaceFactory;
 		private GLWrapper mGLWrapper;
 		private int mDebugFlags;
 
@@ -119,7 +119,7 @@ public class GLWallpaperService extends WallpaperService {
 			return mDebugFlags;
 		}
 
-		public void setRenderer(Renderer renderer) {
+		public void setRenderer(GLSurfaceView.Renderer renderer) {
 			checkRenderThreadState();
 			if (mEGLConfigChooser == null) {
 				mEGLConfigChooser = new SimpleEGLConfigChooser(true);
@@ -134,17 +134,17 @@ public class GLWallpaperService extends WallpaperService {
 			mGLThread.start();
 		}
 
-		public void setEGLContextFactory(EGLContextFactory factory) {
+		public void setEGLContextFactory(GLSurfaceView.EGLContextFactory factory) {
 			checkRenderThreadState();
 			mEGLContextFactory = factory;
 		}
 
-		public void setEGLWindowSurfaceFactory(EGLWindowSurfaceFactory factory) {
+		public void setEGLWindowSurfaceFactory(GLSurfaceView.EGLWindowSurfaceFactory factory) {
 			checkRenderThreadState();
 			mEGLWindowSurfaceFactory = factory;
 		}
 
-		public void setEGLConfigChooser(EGLConfigChooser configChooser) {
+		public void setEGLConfigChooser(GLSurfaceView.EGLConfigChooser configChooser) {
 			checkRenderThreadState();
 			mEGLConfigChooser = configChooser;
 		}
@@ -190,8 +190,13 @@ public class GLWallpaperService extends WallpaperService {
 		}
 	}
 
+	/**
+	 * Empty wrapper for {@link GLSurfaceView.Renderer}.
+	 *
+	 * @deprecated Use {@link GLSurfaceView.Renderer} instead.
+	 */
+	@Deprecated
 	public interface Renderer extends GLSurfaceView.Renderer {
-
 	}
 }
 
@@ -231,19 +236,15 @@ class LogWriter extends Writer {
 // ----------------------------------------------------------------------
 
 /**
- * An interface for customizing the eglCreateContext and eglDestroyContext calls.
- *
-
- * This interface must be implemented by clients wishing to call
- * {@link GLWallpaperService#setEGLContextFactory(EGLContextFactory)}
+ * Empty wrapper for {@link GLSurfaceView.EGLContextFactory}.
+ * 
+ * @deprecated Use {@link GLSurfaceView.EGLContextFactory} instead.
  */
-interface EGLContextFactory {
-	EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig eglConfig);
-
-	void destroyContext(EGL10 egl, EGLDisplay display, EGLContext context);
+@Deprecated
+interface EGLContextFactory extends GLSurfaceView.EGLContextFactory {
 }
 
-class DefaultContextFactory implements EGLContextFactory {
+class DefaultContextFactory implements GLSurfaceView.EGLContextFactory {
 
 	public EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig config) {
 		return egl.eglCreateContext(display, config, EGL10.EGL_NO_CONTEXT, null);
@@ -255,19 +256,15 @@ class DefaultContextFactory implements EGLContextFactory {
 }
 
 /**
- * An interface for customizing the eglCreateWindowSurface and eglDestroySurface calls.
- *
-
- * This interface must be implemented by clients wishing to call
- * {@link GLWallpaperService#setEGLWindowSurfaceFactory(EGLWindowSurfaceFactory)}
+ * Empty wrapper for {@link GLSurfaceView.EGLWindowSurfaceFactory}.
+ * 
+ * @deprecated Use {@link GLSurfaceView.EGLWindowSurfaceFactory} instead.
  */
-interface EGLWindowSurfaceFactory {
-	EGLSurface createWindowSurface(EGL10 egl, EGLDisplay display, EGLConfig config, Object nativeWindow);
-
-	void destroySurface(EGL10 egl, EGLDisplay display, EGLSurface surface);
+@Deprecated
+interface EGLWindowSurfaceFactory extends GLSurfaceView.EGLWindowSurfaceFactory {
 }
 
-class DefaultWindowSurfaceFactory implements EGLWindowSurfaceFactory {
+class DefaultWindowSurfaceFactory implements GLSurfaceView.EGLWindowSurfaceFactory {
 
 	public EGLSurface createWindowSurface(EGL10 egl, EGLDisplay
 			display, EGLConfig config, Object nativeWindow) {
@@ -314,13 +311,13 @@ class EglHelper {
 	private EGLContext mEglContext;
 	EGLConfig mEglConfig;
 
-	private EGLConfigChooser mEGLConfigChooser;
-	private EGLContextFactory mEGLContextFactory;
-	private EGLWindowSurfaceFactory mEGLWindowSurfaceFactory;
+	private GLSurfaceView.EGLConfigChooser mEGLConfigChooser;
+	private GLSurfaceView.EGLContextFactory mEGLContextFactory;
+	private GLSurfaceView.EGLWindowSurfaceFactory mEGLWindowSurfaceFactory;
 	private GLWrapper mGLWrapper;
 
-	public EglHelper(EGLConfigChooser chooser, EGLContextFactory contextFactory,
-			EGLWindowSurfaceFactory surfaceFactory, GLWrapper wrapper) {
+	public EglHelper(GLSurfaceView.EGLConfigChooser chooser, GLSurfaceView.EGLContextFactory contextFactory,
+			GLSurfaceView.EGLWindowSurfaceFactory surfaceFactory, GLWrapper wrapper) {
 		this.mEGLConfigChooser = chooser;
 		this.mEGLContextFactory = contextFactory;
 		this.mEGLWindowSurfaceFactory = surfaceFactory;
@@ -472,9 +469,9 @@ class GLThread extends Thread {
 	private final GLThreadManager sGLThreadManager = new GLThreadManager();
 	private GLThread mEglOwner;
 
-	private EGLConfigChooser mEGLConfigChooser;
-	private EGLContextFactory mEGLContextFactory;
-	private EGLWindowSurfaceFactory mEGLWindowSurfaceFactory;
+	private GLSurfaceView.EGLConfigChooser mEGLConfigChooser;
+	private GLSurfaceView.EGLContextFactory mEGLContextFactory;
+	private GLSurfaceView.EGLWindowSurfaceFactory mEGLWindowSurfaceFactory;
 	private GLWrapper mGLWrapper;
 
 	public SurfaceHolder mHolder;
@@ -494,12 +491,12 @@ class GLThread extends Thread {
 	private boolean mEventsWaiting;
 	// End of member variables protected by the sGLThreadManager monitor.
 
-	private GLWallpaperService.Renderer mRenderer;
+	private GLSurfaceView.Renderer mRenderer;
 	private ArrayList<Runnable> mEventQueue = new ArrayList<Runnable>();
 	private EglHelper mEglHelper;
 
-	GLThread(GLWallpaperService.Renderer renderer, EGLConfigChooser chooser, EGLContextFactory contextFactory,
-			EGLWindowSurfaceFactory surfaceFactory, GLWrapper wrapper) {
+	GLThread(GLSurfaceView.Renderer renderer, GLSurfaceView.EGLConfigChooser chooser, GLSurfaceView.EGLContextFactory contextFactory,
+			GLSurfaceView.EGLWindowSurfaceFactory surfaceFactory, GLWrapper wrapper) {
 		super();
 		mDone = false;
 		mWidth = 0;
@@ -836,11 +833,16 @@ class GLThread extends Thread {
 	}
 }
 
-interface EGLConfigChooser {
-	EGLConfig chooseConfig(EGL10 egl, EGLDisplay display);
+/**
+ * Empty wrapper for {@link GLSurfaceView.EGLConfigChooser}.
+ *
+ * @deprecated Use {@link GLSurfaceView.EGLConfigChooser} instead.
+ */
+@Deprecated
+interface EGLConfigChooser extends GLSurfaceView.EGLConfigChooser {
 }
 
-abstract class BaseConfigChooser implements EGLConfigChooser {
+abstract class BaseConfigChooser implements GLSurfaceView.EGLConfigChooser {
 	public BaseConfigChooser(int[] configSpec) {
 		mConfigSpec = configSpec;
 	}
